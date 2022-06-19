19c9889d610bc5227f8599d8749bace3707a5e78
Morandev <moranfcnddev@gmail.com>
2022-06-19 20:58:50 -0300

    first commit

---
+++ src/main/java/ar/com/digitalhouse/ctd/clinicproject/ClinicprojectApplication.java
@@ -0,0 +1,13 @@
+package ar.com.digitalhouse.ctd.clinicproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/digitalhouse/ctd/clinicproject/ClinicprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.digitalhouse.ctd.clinicproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
